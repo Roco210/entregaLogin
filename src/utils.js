33bb5703031af35a8782f25b3a030840cb4b8807
RicardoF <rickyfernandez210@gmail.com>
2023-09-09 15:44:45 -0300

    entrega de log in ricardo fernandez

---
+++ src/utils.js
@@ -0,0 +1,7 @@
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
+
+
+export const __dirname= dirname(fileURLToPath(import.meta.url))
+
+export const URI ="mongodb+srv://rickyfernandez210:123456.@cluster0.ublusye.mongodb.net/ecomerce?retryWrites=true&w=majority"
